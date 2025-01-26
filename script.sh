3c7c9140b3e4276474a15a8db30ac22b6cacc608
sawyer3399 <46585978+sawyer3399@users.noreply.github.com>
2025-01-25 22:43:59 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 IPs=()
-network_id="10.10"
+network_id="10.20"
 number_of_teams=10
-host_ids=(10 20 30 40 50)
-admin="admin1"
-default_password="Password1!"
+host_ids=(111 121 131)
+admin="wburns"
 timeout_duration=1
 
 update_passwords() {
     read -p "Team to update passwords: " team_number
-    read -p "New password to set: " new_password
+    read -p "Original password: " original_password
+    read -p "New password: " new_password
 
     for IP in "${IPs[@]}"; do
         team_number_from_IP=$(echo "$IP" | cut -d'.' -f3)
@@ -18,10 +18,15 @@ update_passwords() {
         if [[ "$team_number_from_IP" == "$team_number" ]]; then
             echo "Updating passwords for team $team_number on IP: $IP"
 
-            sshpass -p "$default_password" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout_duration "$admin@$IP" "
-                echo \"$default_password\" | sudo -S bash -c '
+            sshpass -p "$orginal_password" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout_duration "$admin@$IP" "
+                echo \"$original_password\" | sudo -S bash -c '
                     for user in \$(cut -d: -f1 /etc/passwd); do
-                        echo \"$user:$new_password\" | sudo -S chpasswd
+                        echo \"\$user:$new_password\" |  chpasswd 
+                        if [[ $? -ne 0 ]]; then
+                            echo \"Password failed to updated for user \$user@$IP\"
+                        else
+                            echo \"Password updated for user \$user@$IP\"
+                        fi
                     done
                 '
             "
@@ -29,18 +34,15 @@ update_passwords() {
     done
 }
 
-
 main() {
-    # Populate the IP list
     for ((team=1; team<=number_of_teams; team++)); do
         for host_id in "${host_ids[@]}"; do
             IPs+=("$network_id.$team.$host_id")
         done
     done
 
-    # Menu loop
     while true; do
-        echo "DAKOTA CONQUEST SCRIPTS"
+        echo "DAKOTA CONQUEST DEFENSIVE SCRIPTS"
         echo "1. Update Passwords"
         echo "2. Exit"
         read -p "Choose an option: " choice
