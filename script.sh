07805c3971c76ef02b686937dc2fc711f0c792e3
sawyer3399 <46585978+sawyer3399@users.noreply.github.com>
2025-02-08 00:59:21 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,42 +1,69 @@
 #!/bin/bash
 
 IPs=()
-network_id="10.20"
-number_of_teams=10
-host_ids=(111 121 131)
-admin="wburns"
-timeout_duration=1
+network_id="1.1"
+number_of_teams=4
+host_ids=(1 2 3 4)
+admin="admin"
+timeout_duration=3
 
-update_passwords() {
-    read -p "Team to update passwords: " team_number
-    read -p "Original password: " original_password
-    read -p "New password: " new_password
+clear_logs() {
+    read -p "Team: " team
+    read -p "Password: " password
 
     for IP in "${IPs[@]}"; do
-        team_number_from_IP=$(echo "$IP" | cut -d'.' -f3)
+        team_from_IP=$(echo "$IP" | cut -d'.' -f3)
+        
+        if [[ "$team_from_IP" == "$team" ]]; then
+            echo "Trying to SSH @ $IP"
+            
+            sshpass -p "$password" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout_duration "$admin@$IP" "
+                echo "$password" | sudo -S bash -c '
+                    cat /dev/null > /var/log/syslog
+                    cat /dev/null > /var/log/auth.log
+                    cat /dev/null > /var/log/kern.log
+                '
 
-        if [[ "$team_number_from_IP" == "$team_number" ]]; then
-            echo "Updating passwords for team $team_number on IP: $IP"
+                echo "Logs cleared successfully @ $IP"
+            "
+
+            if [[ $? -ne 0 ]]; then
+                echo "SSH failed @ $IP"
+            fi
+        fi
+    done
+}
+
+update_passwords() {
+    read -p "Team number: " team_number
+    read -p "Original password: " original_password
+    read -p "Password: " password
+
+    for IP in "${IPs[@]}"; do
+        team_from_IP=$(echo "$IP" | cut -d'.' -f3)
+
+        if [[ "$team_from_IP" == "$team" ]]; then
+            echo "Trying to SSH @ $IP"
 
             sshpass -p "$original_password" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout_duration "$admin@$IP" "
-                echo \"$original_password\" | sudo -S rm -f ~/.ssh/authorized_keys
-                
-                echo \"$original_password\" | sudo -S bash -c '
-                    for user in \$(cut -d: -f1 /etc/passwd); do
-                        echo \"\$user:$new_password\" | chpasswd 
+                echo "$original_password" | sudo -S rm -f ~/.ssh/authorized_keys
+
+                echo "$original_password" | sudo -S bash -c '
+                    for user in $(cut -d: -f1 /etc/passwd); do
+                        echo "$user:$password" | chpasswd 
                         
                         if [[ $? -ne 0 ]]; then
-                            echo \"Password failed to updated for user \$user@$IP\"
+                            echo "Password failed to update for $user @ $IP"
                         else
-                            echo \"Password updated for user \$user@$IP\"
+                            echo "Password successfully updated for $user @ $IP"
                         fi
                     done
                 '
-
-                echo \"$new_password\" | sudo -S history -c
-                echo \"$new_password\" | sudo -S rm -f /var/log/auth.log
-                echo \"$new_password\" | sudo -S rm -f /var/log/messages
             "
+
+            if [[ $? -ne 0 ]]; then
+                echo "SSH failed @ $IP"
+            fi
         fi
     done
 }
@@ -51,12 +78,12 @@ main() {
     while true; do
         echo "DAKOTA CONQUEST DEFENSIVE SCRIPTS"
         echo "1. Update Passwords"
-        echo "2. Exit"
+        echo "2. Clear Logs"
         read -p "Choose an option: " choice
 
         case $choice in
             1) update_passwords ;;
-            2) break ;;
+            2) clear_logs ;;
             *) echo "Invalid choice. Please try again." ;;
         esac
     done
