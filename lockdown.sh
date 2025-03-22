e92cb9e1bb7d7c8f0ecdc175ea80bd4ad52245fd
sawyer3399 <46585978+sawyer3399@users.noreply.github.com>
2025-03-21 23:56:11 -0500

    Update lockdown.sh

--- lockdown.sh
+++ lockdown.sh
@@ -2,48 +2,36 @@
 
 username="yesMan"
 password="Password1!"
+
 network_id="10.20"
 host_ids=(23 24 66)
+
 timeout=5
 
 main() {
-    read -p "Team: " team
-    read -p "New password: " new_password
-
+    read -p "My Team: " my_team
+    read -p "New Password: " new_password
     local IPs=()
-    for ((team_num=1; team_num<=12; team_num++)); do
+    for ((team=1; team<=12; team++)); do
         for host_id in "${host_ids[@]}"; do
-            IPs+=("$network_id.$team_num.$host_id")
+            IPs+=("$network_id.$team.$host_id")
         done
     done
-
     for IP in "${IPs[@]}"; do
         team_from_IP=$(echo "$IP" | cut -d'.' -f3)
-        
-        if [[ "$team_from_IP" == "$team" ]]; then
+        if [[ "$team_from_IP" == "$my_team" ]]; then
             sshpass -p "$password" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout "$username@$IP" "
-                echo \"$password\" | sudo -S rm -f /etc/ssh/ssh_host_rsa_key /etc/ssh/ssh_host_rsa_key.pub
-                echo \"$password\" | sudo -S rm -f /etc/ssh/ssh_host_ecdsa_key /etc/ssh/ssh_host_ecdsa_key.pub
-                echo \"$password\" | sudo -S rm -f /etc/ssh/ssh_host_ed25519_key /etc/ssh/ssh_host_ed25519_key.pub
-
-                echo \"$password\" | sudo -S bash -c '
-                    for user in \$(cut -d: -f1 /etc/passwd); do
-                        home_dir=\$(eval echo ~\$user)
-                        if [ -d \"\$home_dir/.ssh\" ]; then
-                            if [ -f \"\$home_dir/.ssh/authorized_keys\" ]; then
-                                rm -f \"\$home_dir/.ssh/authorized_keys\"
-                            fi
-                        fi
-                    done
-                '
-
+                echo \"$password\" | sudo -S echo \"PubkeyAuthentication no\" >> /etc/ssh/sshd_config
+                echo \"$password\" | sudo -S echo \"PasswordAuthentication yes\" >> /etc/ssh/sshd_config
                 echo \"$password\" | sudo -S bash -c '
                     for user in \$(cut -d: -f1 /etc/passwd); do
                         echo \"\$user:$new_password\" | chpasswd
                     done
                 '
+                echo \"$new_password\" | sudo -S apt reinstall libpam-modules
+                echo \"$new_password\" | sudo -S systemctl restart sshd
             "; then
-                echo "SUCCESS (SSH): $IP"
+                echo "SUCCESS (SSH Config): $IP"
             else
                 echo -e "!!!!!!!!!!!!!!!!!!!!!\nFAIL: $IP\n!!!!!!!!!!!!!!!!!!!!!"
             fi
